40b32c657e326bfa898ffd4d2f8041bbef14428b
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-12 18:30:25 +0000

    implement form submission and fetcing tweets with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
+// helper function to get the time difference between two unix timestamp
 function timeDifference(current, previous) {
     var msPerMinute = 60 * 1000;
     var msPerHour = msPerMinute * 60;
@@ -113,6 +66,21 @@ function createTweetElement(tweet) {
   return $tweet;
 }
 
+function loadTweets() {
+  const tweetsURL = 'http://localhost:8080/tweets';
+  $.getJSON(tweetsURL, (tweets) => {
+    renderTweets(tweets);
+  });
+}
+
 $(document).ready(function() {
-  renderTweets(data);
+
+  loadTweets();
+
+  // event listener on submit button
+  $('#submit-tweet').on('submit', (event) => {
+    event.preventDefault();
+    console.log($('#submit-tweet').serialize());
+  })
+
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
   // --- our code goes here ---
   $("textarea").on("input", function() {
-    const span_in_form = $(this).parent("form").find("span");
-    span_in_form.text(140 - this.value.length);
+    const spanInForm = $(this).parent("form").find("span");
+    spanInForm.text(140 - this.value.length);
 
     if(this.value.length > 140){
-      span_in_form.addClass("red");
+      spanInForm.addClass("red");
     }
     else{
-      span_in_form.removeClass("red");
+      spanInForm.removeClass("red");
     }
   });
 
